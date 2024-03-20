ef7cd9d7f7c83d19dec2dfbbcc0723a978c9ef81
Vito Gautama <vito.gautama@ti.ukdw.ac.id>
2024-03-20 07:19:06 +0700

    done

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float time;
+    public float timer;
+
+    // Update is called once per frame
+    void Start()
+    {
+        time = 2;
+        timer = time;
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer<0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = time;
+        }
+    }
+}
